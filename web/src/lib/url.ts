dced02db0b13941a61579435c1defe8de2cae7b8
rainbowmori <rainbowmori1218@gmail.com>
2025-10-19 10:16:18 +0900

    fix: use correct base url

--- web/src/lib/url.ts
+++ web/src/lib/url.ts
@@ -1,13 +1,13 @@
+import { base } from '$app/paths';
+
+const baseUrl = base ? base.endsWith('/') ? base : `${base}/` : '/';
+
 export function assetUrl(path: string): string {
-  let base = import.meta.env.BASE_URL || '/';
-  if (!base.endsWith('/')) {
-    base += '/';
-  }
   if (path.startsWith('/')) {
     path = path.slice(1);
   }
-  if (base === '' || base === '/') {
+  if (baseUrl === '' || baseUrl === '/') {
     return path;
   }
-  return base + path;
+  return baseUrl + path;
 }
